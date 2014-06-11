7f7dea114b47462d3f9851d47f3e0a28c2aa1763
Nick Shelton <nick@bottomlineperformance.com>
2014-06-11 19:45:46 -0400

    uncommented and commented stuff

--- src/app/DataModel.js
+++ src/app/DataModel.js
@@ -2,17 +2,17 @@
  * Created by teisaacs on 5/30/14.
  */
 
-//var app = angular.module('devworkshop.DataModel', []);
+var app = angular.module('devworkshop.DataModel', []);
 
 /**
 * Application Data Model for shared data across application
 */
-//app.factory('Model', function(){
-//    var appModel = {};
-//
-//    appModel.myFunction = function() {
-//        alert("Hello Data Model");
-//    };
-//
-//    return appModel;
-//});
+app.factory('Model', function(){
+   var appModel = {};
+
+   appModel.myFunction = function() {
+       alert("Hello Data Model");
+   };
+
+   return appModel;
+});
